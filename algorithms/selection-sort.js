da30ea4f2573ae082c3e4b58572f27c8184ab2d9
Bamdad Erfanian <erfanianbamdad@gmail.com>
2024-03-06 09:24:16 -0500

    add selection

---
+++ algorithms/selection-sort.js
@@ -0,0 +1,12 @@
+/*
+Q. Given an unsorted array, perform selection sort in ascending order.
+
+Selection sort iterates through an array and grows a sorted section. At each iteration, it finds the smallest value remaining from the unsorted section, and adds it to the end of the sorted section. It repeats until no input elements remain.
+
+This is normally done in place, without using extra memory.
+
+Examples:
+
+Given an array: [3, 1, 2, 4] // returns [1, 2, 3, 4]
+Given an array: [1] // returns [1]
+*/
