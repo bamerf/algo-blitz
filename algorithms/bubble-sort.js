4896e2416dcbfcad4be4fdaaaeec91c5d28462db
Bamdad Erfanian <erfanianbamdad@gmail.com>
2024-03-06 09:24:03 -0500

    add bubble

---
+++ algorithms/bubble-sort.js
@@ -0,0 +1,10 @@
+/*
+Given an unsorted array, perform bubble sort in ascending order.
+
+Bubble sort works by swapping adjacent pairs of elements that are out of order. At every pass through the array, this process causes the largest remaining misplaced element to bubble into position.
+
+Examples:
+
+Given an array: [3, 1, 2, 4] // returns [1, 2, 3, 4]
+Given an array: [1] // returns [1]
+*/
