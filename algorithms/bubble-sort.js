98e74f13bece978dd63086beb50cec5174176b69
Bamdad Erfanian <erfanianbamdad@gmail.com>
2024-04-02 11:22:15 -0600

    add bubble

--- algorithms/bubble-sort.js
+++ algorithms/bubble-sort.js
@@ -8,3 +8,22 @@ Examples:
 Given an array: [3, 1, 2, 4] // returns [1, 2, 3, 4]
 Given an array: [1] // returns [1]
 */
+
+const bubbleSort = (arr) => {
+  let swapped = true;
+
+  while (swapped) {
+    swapped = false;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  }
+
+  return arr;
+};
